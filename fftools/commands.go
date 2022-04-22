3a892af7cde247665ed013bdde6ab86a60f45c13
ohzqq <iamchurkey@gmail.com>
2022-04-22 17:58:22 -0400

    Refactor

--- fftools/commands.go
+++ fftools/commands.go
@@ -2,6 +2,11 @@ package fftools
 
 import (
 	"fmt"
+	"io/fs"
+	"os"
+	"log"
+	"strings"
+	"path/filepath"
 )
 
 func (m *Media) Split() {
@@ -24,5 +29,49 @@ func (m *Media) Cut(ss, to string, no int) {
 	}
 	cmd.Args().PostInput(timestamps).Out("tmp" + count).Ext(m.Ext)
 	cmd.Run()
+}
+
+func Join(ext string) *FFmpegCmd {
+	ff := NewCmd()
+	files := find(ext)
+	ff.tmpFile = concatFile(files)
+	ff.In(NewMedia(ff.tmpFile.Name()))
+	pre := flagArgs{"f": "concat", "safe": "0"}
+	ff.Args().PreInput(pre).Ext(ext)
+	return ff
+}
+
+func concatFile(files []string) *os.File {
+	file, err := os.CreateTemp("", "audiofiles")
+	if err != nil { log.Fatal(err) }
+
+	var cat strings.Builder
+	for _, f := range files {
+		abs, err := filepath.Abs(f)
+		if err != nil { log.Fatal(err) }
+		cat.WriteString("file ")
+		cat.WriteString("'")
+		cat.WriteString(abs)
+		cat.WriteString("'")
+		cat.WriteString("\n")
+	}
+
+	if _, err := file.WriteString(cat.String()); err != nil {
+		log.Fatal(err)
+	}
+
+	return file
+}
+
+func find(ext string) []string {
+	var files []string
+	filepath.WalkDir(".", func(file string, dir fs.DirEntry, e error) error {
+		if e != nil { return e }
+		if filepath.Ext(dir.Name()) == ext {
+			files = append(files, file)
+		}
+		return nil
+	})
+	return files
 }
 
