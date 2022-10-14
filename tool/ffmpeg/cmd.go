993aeef254334d95b7ce8cd511d29508517d0377
ohzqq <iamchurkey@gmail.com>
2022-10-14 12:56:56 -0400

    input

--- tool/ffmpeg/cmd.go
+++ tool/ffmpeg/cmd.go
@@ -2,7 +2,6 @@ package ffmpeg
 
 import (
 	"strconv"
-	"strings"
 )
 
 type Cmd struct {
@@ -52,40 +51,6 @@ func (i Input) Map() []string {
 	}
 
 	return input
-}
-
-type Filters []Filter
-
-func (f Filters) String() string {
-	var filters []string
-	for _, f := range f {
-		filters = append(filters, f.String())
-	}
-	return strings.Join(filters, ",")
-}
-
-type Filter struct {
-	Name   string
-	Params [][]string
-}
-
-func NewFilter(name string) Filter {
-	return Filter{
-		Name: name,
-	}
-}
-
-func (f *Filter) Set(val ...string) {
-	f.Params = append(f.Params, val)
-}
-
-func (f Filter) String() string {
-	var params []string
-	for _, val := range f.Params {
-		params = append(params, strings.Join(val, "="))
-	}
-	p := strings.Join(params, ":")
-	return f.Name + "=" + p
 }
 
 func New() *Cmd {
