07ccfc2125456c85071e2f9e0d30dcc678679201
ohzqq <iamchurkey@gmail.com>
2022-10-22 11:56:03 -0400

    stuff

--- cmd/show.go
+++ cmd/show.go
@@ -1,9 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-
-	"github.com/ohzqq/avtools/ffmpeg"
 	"github.com/ohzqq/avtools/tool"
 	"github.com/spf13/cobra"
 )
@@ -16,47 +13,18 @@ var showCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		input := args[0]
 		flag.Args.Input = input
+		c := tool.NewCmd()
+		c.ParseFlags(flag)
 		//ff := media.Cfg().Profiles["gif"].FFmpegCmd()
 		//ff.Input(input)
 
 		//fmt.Printf("%+V\n", media.Cfg().ListProfiles())
-		medias(input)
 	},
-}
-
-func medias(input string) {
-	//c := media.NewMedia(input)
-	c := tool.NewCmd().SetInput(input).ParseFlags(flag)
-	ff := c.FFmpeg()
-	eq := ffmpeg.NewFilter("eq")
-	eq.Set("brightness", "1.0")
-	fps := ffmpeg.NewFilter("fps")
-	fps.Set("60")
-	ff.SetVideoCodec("libx264").
-		AppendVideoFilter(eq).
-		AppendVideoFilter(fps)
-
-	c.Add(ff)
-	fmt.Printf("%+V\n", c.Batch[0].String())
 }
 
 func init() {
 	rootCmd.AddCommand(showCmd)
-	showCmd.PersistentFlags().StringVarP(&flag.Args.Output, "output", "o", "", "update ffmetadata")
-	showCmd.PersistentFlags().StringVarP(&flag.Args.Meta, "ffmeta", "f", "", "update ffmetadata")
-	showCmd.PersistentFlags().StringVarP(&flag.Args.Cover, "album-art", "a", "", "update album art")
-	showCmd.PersistentFlags().StringVarP(&flag.Args.Cue, "cuesheet", "c", "", "update chapter metadata")
 	showCmd.PersistentFlags().BoolVarP(&flag.Bool.Meta, "meta", "m", false, "print ffmeta")
 	showCmd.PersistentFlags().BoolVarP(&flag.Bool.Cue, "cue", "C", false, "print cue sheet")
 	showCmd.PersistentFlags().BoolVarP(&flag.Bool.Json, "json", "j", false, "print json")
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// showCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// showCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
