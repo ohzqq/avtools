951bd4bbf888b930a7914575d2fcca298d9c8737
ohzqq <iamchurkey@gmail.com>
2022-12-30 14:06:50 -0500

    chapters

--- ffprobe.go
+++ ffprobe.go
@@ -7,6 +7,34 @@ import (
 	ffmpeg "github.com/u2takey/ffmpeg-go"
 )
 
+type ProbeMeta struct {
+	name        string
+	Streams     []*StreamEntry `json:"streams"`
+	FormatEntry `json:"format"`
+	Chaps       []ChapterEntry `json:"chapters"`
+}
+
+type StreamEntry struct {
+	CodecName string `json:"codec_name"`
+	CodecType string `json:"codec_type"`
+}
+
+type FormatEntry struct {
+	Filename string            `json:"filename"`
+	Dur      string            `json:"duration"`
+	Size     string            `json:"size"`
+	BitRate  string            `json:"bit_rate"`
+	Tags     map[string]string `json:"tags"`
+}
+
+type ChapterEntry struct {
+	Base         string            `json:"time_base",ini:"timebase"`
+	StartTime    int               `json:"start",ini:"start"`
+	EndTime      int               `json:"end",ini:"end"`
+	ChapterTitle string            `json:"title", ini:"title"`
+	Tags         map[string]string `json:"tags"`
+}
+
 var probeArgs = []ffmpeg.KwArgs{
 	ffmpeg.KwArgs{"show_chapters": ""},
 	ffmpeg.KwArgs{"select_streams": "a"},
