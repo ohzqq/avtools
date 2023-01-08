d7be87c7f2c43550efd83e42fd76300c306f5bef
ohzqq <iamchurkey@gmail.com>
2023-01-08 16:43:14 -0500

    join

--- cmd/cmd/join.go
+++ cmd/cmd/join.go
@@ -1,8 +1,9 @@
 package cmd
 
 import (
-	"fmt"
+	"log"
 
+	"github.com/ohzqq/avtools/media"
 	"github.com/spf13/cobra"
 )
 
@@ -11,20 +12,23 @@ var joinCmd = &cobra.Command{
 	Use:   "join",
 	Short: "join media files",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("join called")
+		dir := "."
+		var ext string
+		switch len(args) {
+		case 2:
+			dir = args[1]
+			fallthrough
+		case 1:
+			ext = args[0]
+		default:
+			log.Fatalf("wrong number of args")
+		}
+
+		c := media.Join(ext, dir)
+		c.Run()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(joinCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// joinCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// joinCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
---
+++ media/join.go
@@ -0,0 +1,95 @@
+package media
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+
+	"github.com/ohzqq/avtools"
+	"github.com/ohzqq/avtools/ff"
+)
+
+type JoinCmd struct {
+	dir    string
+	ext    string
+	ffmeta string
+}
+
+func Join(ext string, dir ...string) Cmd {
+	d := "."
+	if len(dir) > 0 {
+		d = dir[0]
+	}
+
+	path, err := filepath.Abs(d)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	files, err := filepath.Glob(path + "/*" + ext)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tmp, err := os.CreateTemp("", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tmp.Close()
+
+	var media []*Media
+	for _, f := range files {
+		media = append(media, New(f))
+		str := "file '" + f + "'\n"
+		if _, err := tmp.WriteString(str); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	cmd := ff.New()
+	cmd.In(tmp.Name())
+	cmd.Input.Set("f", "concat")
+	cmd.Input.Set("safe", "0")
+	cmd.Input.Set("y", "")
+	base := filepath.Base(d)
+	name := filepath.Join(path, base)
+	cmd.Output.Set("c", "copy").Ext(ext).Name(name)
+	if media[0].HasCover {
+		cmd.Output.Set("vn", "")
+	}
+
+	tmpMedia := media[0]
+	tmpMedia.Chapters = CalculateChapters(media)
+	s := tmpMedia.SaveMetaFmt("ini")
+	s.Run()
+
+	return cmd
+}
+
+func CalculateChapters(media []*Media) []*avtools.Chapter {
+	var chapters []*avtools.Chapter
+
+	var start = []int64{0}
+	var end []int64
+	for idx, m := range media {
+		d := m.GetTag("duration")
+		dur := avtools.ParseStamp(d)
+		e := start[idx] + dur.Milliseconds()
+		end = append(end, e)
+		if idx < len(media)-1 {
+			s := end[idx]
+			start = append(start, s)
+		}
+		ss := avtools.ParseStampDuration(start[idx], 1000)
+		to := avtools.ParseStampDuration(end[idx], 1000)
+		chapter := &avtools.Chapter{
+			Start: avtools.Timestamp(ss),
+			End:   avtools.Timestamp(to),
+			Title: "Chapter " + strconv.Itoa(idx+1),
+		}
+		chapters = append(chapters, chapter)
+	}
+
+	return chapters
+}
