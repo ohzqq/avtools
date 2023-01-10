5806fd54e6e3e8c51d6658bd9775fc73686afffa
ohzqq <iamchurkey@gmail.com>
2023-01-10 13:34:21 -0500

    consolidate commands

--- media/command.go
+++ media/command.go
@@ -2,9 +2,14 @@ package media
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"path/filepath"
+	"strconv"
 
 	"github.com/ohzqq/avtools"
+	"github.com/ohzqq/avtools/ff"
+	ffmpeg "github.com/u2takey/ffmpeg-go"
 )
 
 type Cmd interface {
@@ -100,6 +105,132 @@ func (cmd Command) Extract(input string) []Cmd {
 	return cmds
 }
 
+func Join(ext string, dir ...string) (Cmd, map[string]Cmd) {
+	d := "."
+	if len(dir) > 0 {
+		d = dir[0]
+	}
+
+	path, err := filepath.Abs(d)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	files, err := filepath.Glob(path + "/*" + ext)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tmp, err := os.CreateTemp("", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tmp.Close()
+
+	var media []*Media
+	for _, f := range files {
+		media = append(media, New(f))
+		str := "file '" + f + "'\n"
+		if _, err := tmp.WriteString(str); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	formats := make(map[string]Cmd)
+	tmpMedia := media[0]
+	tmpMedia.SetChapters(GenerateChapters(media))
+	formats["ini"] = tmpMedia.SaveMetaFmt("ini")
+	formats["cue"] = tmpMedia.SaveMetaFmt("cue")
+
+	cmd := ff.New()
+	cmd.In(tmp.Name())
+	cmd.Input.Set("f", "concat")
+	cmd.Input.Set("safe", "0")
+	cmd.Input.Set("y", "")
+
+	if media[0].HasCover {
+		cmd.Output.Set("vn", "")
+	}
+
+	base := filepath.Base(d)
+	name := filepath.Join(path, base)
+	cmd.Output.Set("c", "copy").Ext(ext).Name(name)
+
+	return cmd.Compile(), formats
+}
+
+func GenerateChapters(media []*Media) []*avtools.Chapter {
+	var chapters []*avtools.Chapter
+
+	var start = []int64{0}
+	var end []int64
+	for idx, m := range media {
+		d := m.GetTag("duration")
+		dur := avtools.ParseStamp(d)
+		e := start[idx] + dur.Milliseconds()
+		end = append(end, e)
+		if idx < len(media)-1 {
+			s := end[idx]
+			start = append(start, s)
+		}
+		ss := avtools.ParseStampDuration(start[idx], 1000)
+		to := avtools.ParseStampDuration(end[idx], 1000)
+		chapter := &avtools.Chapter{
+			Start: avtools.Timestamp(ss),
+			End:   avtools.Timestamp(to),
+			Title: "Chapter " + strconv.Itoa(idx+1),
+		}
+		chapters = append(chapters, chapter)
+	}
+
+	return chapters
+}
+func ExtractCover(m *Media) Cmd {
+	var stream Stream
+	for _, s := range m.VideoStreams() {
+		if s.IsCover {
+			stream = s
+		}
+		break
+	}
+	cmd := ff.New()
+	cmd.In(m.Input.Abs, ffmpeg.KwArgs{"y": ""})
+
+	name := m.Input.NewName()
+	n := name.Prefix("cover-").Join()
+	cmd.Output.Name(n)
+	cmd.Output.Pad("").Set("c", "copy").Set("an", "")
+
+	switch stream.CodecName {
+	case "mjpeg":
+		cmd.Ext(".jpg")
+	case "png":
+		cmd.Ext(".png")
+	}
+
+	return cmd.Compile()
+}
+
+func (m Media) SaveMetaFmt(f string) Cmd {
+	var cmd Cmd
+	switch f {
+	case "ini":
+		name := m.Input.NewName()
+		file := name.WithExt(".ini")
+		file.Save(m.DumpIni())
+		cmd = file
+	case "ffmeta":
+		cmd = m.DumpFFMeta()
+	case "cue":
+		if m.HasChapters() {
+			name := m.Input.NewName()
+			file := name.WithExt(".cue")
+			file.Save(m.DumpCue())
+			cmd = file
+		}
+	}
+	return cmd
+}
 func (cmd Command) CutStamp(input, start, end string) Cmd {
 	var (
 		chapter = &avtools.Chapter{}
